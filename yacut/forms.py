18bbe8e05b911ca51ba0cbff3e9fcffc30e054a3
Michael R <rodichevmichael@gmail.com>
2024-02-29 13:57:14 +0300

    added URLMapForm

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,22 +1,30 @@
 from flask_wtf import FlaskForm
-from wtforms import URLField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms.fields.simple import StringField, SubmitField, URLField
+from wtforms.validators import DataRequired, Length, Optional, Regexp, URL
 
 from settings import MAX_LENGHT_USER_URL
 
 
-class URLMap(FlaskForm):
+class URLMapForm(FlaskForm):
     original = URLField(
         'Длинная ссылка',
         validators=[
-            DataRequired(message='Это поле обязательно для заполнения')]
+            DataRequired(message='Это поле обязательно для заполнения'),
+            URL(message='Введите ссылку в корректном формате')
+        ]
     )
-    short = URLField(
+    short = StringField(
         'Ваш вариант короткой ссылки',
         validators=[
             Optional(),
             Length(
                 max=MAX_LENGHT_USER_URL,
-                message=f'Количество символов не больше {MAX_LENGHT_USER_URL}')
+                message=f'Количество символов больше {MAX_LENGHT_USER_URL}'
+            ),
+            Regexp(
+                regex='[a-zA-Z0-9]',
+                message='Ссылка содержит недопустимые символы'
+            )
         ]
     )
+    submit = SubmitField('Создать')
