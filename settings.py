18db1ca11a8e5957d861f1583e64cd3dcbb6f886
michaelrodichev <rodichevmichael@gmail.com>
2024-06-25 09:50:16 +0300

    settings.py

--- settings.py
+++ settings.py
@@ -1,11 +1,16 @@
 import os
+import re
+from string import ascii_letters, digits
+
+
+ALLOWED_SYMBOLS = ascii_letters + digits
+REGEXP = r'[' + re.escape(ALLOWED_SYMBOLS) + r']+'
+MAX_URL_SIZE = 2048
+MAX_SHORT_ID_SIZE = 16
+DEFAULT_SHORT_ID_SIZE = 6
 
 
 class Config(object):
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-#  Константы
-MAX_LENGHT_USER_URL = 6
+    SECRET_KEY = os.getenv('SECRET_KEY', 'MY_SECRET_KEY')
